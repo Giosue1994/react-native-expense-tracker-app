00b0b70919bff626cfa8c2135710319e197fa2d8
Giosuè Lentini <63297733+Giosue1994@users.noreply.github.com>
2024-04-20 13:27:13 +0200

    screens & main navigation

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+import AllExpensesScreen from "./screens/AllExpensesScreen";
+import RecentExpensesScreen from "./screens/RecentExpensesScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Tab.Navigator style={styles.container}>
+          <Tab.Screen
+            name="RecentExpenses"
+            component={RecentExpensesScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="hourglass" size={size} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="AllExpenses"
+            component={AllExpensesScreen}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="calendar" size={size} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/AllExpensesScreen.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function AllExpensesScreen() {
+  return (
+    <View>
+      <Text>AllExpensesScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
