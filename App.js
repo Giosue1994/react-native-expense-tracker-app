26ff744fbfcaedf4f0ab4b29e4d9d0d30ea0adc3
Giosuè Lentini <63297733+Giosue1994@users.noreply.github.com>
2024-04-20 14:39:50 +0200

    styles & nested navigation

--- App.js
+++ App.js
@@ -1,39 +1,89 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 
+import { GlobalStyles } from "./styles";
 import AllExpensesScreen from "./screens/AllExpensesScreen";
 import RecentExpensesScreen from "./screens/RecentExpensesScreen";
+import ManageExpenseScreen from "./screens/ManageExpenseScreen";
 
-const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+const Colors = GlobalStyles.colors;
+
+function BottomTabsNavigator() {
+  return (
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: Colors.primary500,
+        },
+        headerTintColor: "white",
+        tabBarActiveTintColor: Colors.accent500,
+        tabBarInactiveTintColor: Colors.primary200,
+        tabBarStyle: {
+          backgroundColor: Colors.primary500,
+        },
+        headerRight: () => {
+          return (
+            <Pressable onPress={console.log("pressed")}>
+              <Ionicons name="add" size={24} color="white" />
+            </Pressable>
+          );
+        },
+      }}
+    >
+      <BottomTabs.Screen
+        name="RecentExpenses"
+        component={RecentExpensesScreen}
+        options={{
+          title: "Recent Expenses",
+          tabBarLabel: "Recent",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="hourglass" size={size} color={color} />
+          ),
+        }}
+      />
+      <BottomTabs.Screen
+        name="AllExpenses"
+        component={AllExpensesScreen}
+        options={{
+          title: "All Expenses",
+          tabBarLabel: "All Expenses",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar" size={size} color={color} />
+          ),
+        }}
+      />
+    </BottomTabs.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
       <NavigationContainer>
-        <Tab.Navigator style={styles.container}>
-          <Tab.Screen
-            name="RecentExpenses"
-            component={RecentExpensesScreen}
+        <Stack.Navigator
+          screenOptions={{
+            contentStyle: {
+              backgroundColor: Colors.primary700,
+            },
+          }}
+        >
+          <Stack.Screen
+            name="BottomTabs"
+            component={BottomTabsNavigator}
             options={{
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="hourglass" size={size} color={color} />
-              ),
+              headerShown: false,
             }}
           />
-          <Tab.Screen
-            name="AllExpenses"
-            component={AllExpensesScreen}
-            options={{
-              tabBarIcon: ({ color, size }) => (
-                <Ionicons name="calendar" size={size} color={color} />
-              ),
-            }}
-          />
-        </Tab.Navigator>
+          <Stack.Screen name="ManageExpense" component={ManageExpenseScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
     </>
   );
---
+++ screens/ManageExpenseScreen.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function ManageExpenseScreen() {
+  return (
+    <View>
+      <Text>ManageExpenseScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
